81942bad82e1e143ff61e4495a41c75bfb680323
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2022-08-07 02:19:57 +0100

    Update main.h

--- main.h
+++ main.h
@@ -21,7 +21,9 @@ int print_rev(va_list l, flags_t *f);
 int print_rot13(va_list l, flags_t *f);
 int print_percent(va_list l, flags_t *f);
 int print_address(va_list l, flags_t *f);
-int get_flag(char ch, flags_t *f);
+int print_width(const char *format, int *i, va_list list);
+int print_prec(const char *format, int *i, va_list list);
+int print_flags(const char *format, int *i);
 
 /**
   * struct code_format - Struct format
