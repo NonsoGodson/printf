1f3d13754bd44f331523ae5ca87f9042aef3a84c
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2022-08-07 19:46:54 +0100

    Update main.h

--- main.h
+++ main.h
@@ -51,5 +51,6 @@ int (*find_function(const char *format))(va_list)
 int (*get_print(char s))(va_list, flags_t *);
 int print_flag(char s, flags_t *f);
 int print_percent(va_list l, flags_t *f);
+int print_width(const char *format, int *i, va_list list)
 
 #endif /* MAIN_H */
