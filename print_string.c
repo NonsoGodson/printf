4ee54746c2bca3c209e667d6f6d0e3793406ae25
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2022-08-07 22:20:25 +0100

    Update print_string.c

--- print_string.c
+++ print_string.c
@@ -21,3 +21,18 @@ int print_string(va_list l, flags_t *f)
 		s = "(null)";
 	return (_puts(s));
 }
+
+/**
+ * print_char - prints a character
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
+ * Return: number of char printed
+ */
+
+int print_char(va_list l, flags_t *f)
+{
+	(void)f;
+	_putchar(va_arg(l, int));
+	return (1);
+}
